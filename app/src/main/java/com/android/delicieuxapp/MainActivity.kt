ddfe583b31522f0977cc5ce1755bf9a2bf7836a1
Ammar3012 <50088752+Ammar3012@users.noreply.github.com>
2020-12-10 07:04:10 +0700

    pop up

--- app/src/main/java/com/android/delicieuxapp/MainActivity.kt
+++ app/src/main/java/com/android/delicieuxapp/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.android.delicieuxapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.PopupMenu
+import android.widget.PopupWindow
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
         setContentView(R.layout.activity_main)
+        dropdown.setOnClickListener {
+          val window = PopupWindow(this)
+          val view = layoutInflater.inflate(R.layout.popup_search,null)
+          window.contentView = view
+          val search_button = view.findViewById<Button>(R.id.search_button)
+          search_button.setOnClickListener {
+              window.dismiss()
+          }
+
+        }
     }
 }
