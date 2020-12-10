b1a58c567e123824830bc63fb73ce9d85ed3b7c9
Jessicawie <jwienadi@gmail.com>
2020-12-10 09:06:58 +0700

    bottombar

--- app/src/main/java/com/android/delicieuxapp/DetailResto.kt
+++ app/src/main/java/com/android/delicieuxapp/DetailResto.kt
@@ -1,4 +1,22 @@
 package com.android.delicieuxapp
 
-class DetailResto {
-}
+import androidx.appcompat.app.AppCompatActivity
+//import android.R
+import android.os.Bundle
+import android.view.View
+import com.roughike.bottombar.BottomBar
+
+
+    class DetailResto : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.detail_resto)
+            val bottomBar = findViewById<View>(R.id.bottomBar) as BottomBar
+            bottomBar.setOnTabSelectListener { tabId ->
+                if (tabId == R.id.tab_About) {
+                    // The tab with id R.id.tab_favorites was selected,
+                    // change your content accordingly.
+                }
+            }
+        }
+    }
