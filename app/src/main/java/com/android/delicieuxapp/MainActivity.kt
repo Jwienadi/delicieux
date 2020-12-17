b34a442619167af48a5699d6e9854c03db8c5339
Ammar3012 <50088752+Ammar3012@users.noreply.github.com>
2020-12-17 09:56:31 +0700

    sedikir

--- app/src/main/java/com/android/delicieuxapp/MainActivity.kt
+++ app/src/main/java/com/android/delicieuxapp/MainActivity.kt
@@ -16,17 +16,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btdropdown.setOnClickListener {
-          val window = PopupWindow(this)
-          val view = layoutInflater.inflate(R.layout.popup_search,null)
-          window.contentView = view
-          val btsearch = view.findViewById<Button>(R.id.btsearch)
-          btsearch.setOnClickListener {
-              window.dismiss()
-          }
-            window.showAtLocation(homepage,
-                Gravity.CENTER,0,0)
-        }
     }
 
 }
