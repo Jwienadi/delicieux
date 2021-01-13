3c15cb13a0d51f83f808239f3770f25c21f7f7e8
Jessicawie <jwienadi@gmail.com>
2021-01-13 18:58:31 +0700

    splashscreen done

---
+++ app/src/main/java/com/android/delicieuxapp/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.android.delicieuxapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // langsung pindah ke MainActivity atau activity lain
+        // begitu memasuki splash screen ini
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
