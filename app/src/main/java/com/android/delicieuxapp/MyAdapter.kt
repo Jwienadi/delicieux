8cf7dd5a806b8f0c49c8026082033220a127924a
Jessicawie <jwienadi@gmail.com>
2021-01-13 20:13:54 +0700

    a

--- app/src/main/java/com/android/delicieuxapp/MyAdapter.kt
+++ app/src/main/java/com/android/delicieuxapp/MyAdapter.kt
@@ -62,10 +62,10 @@ class MyAdapter (private var articles:MutableList<Restaurant>) : RecyclerView.Ad
         }
 
 
-
+//jwhere
         holder.itemView.setOnClickListener {
             Toast.makeText(context, name, Toast.LENGTH_SHORT).show()
-            var i = Intent(context, DetailResto::class.java)
+            var i = Intent(context, RestoMain::class.java)
             i.putExtra("id", kelci)
             context.startActivity(i)
 
