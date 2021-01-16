7a739dd901383b37edd93542a164c08ce92788f1
daraldrlh <50088698+daraldrlh@users.noreply.github.com>
2021-01-16 14:56:35 +0800

    belum jalan

--- app/src/main/java/com/android/delicieuxapp/MenuResto.kt
+++ app/src/main/java/com/android/delicieuxapp/MenuResto.kt
@@ -3,35 +3,65 @@ package com.android.delicieuxapp
 import android.os.Bundle
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.android.delicieuxapp.API.Api
+import com.android.delicieuxapp.API.RestaurantInfoService
+import com.android.delicieuxapp.API.RestoDetailResponse
 import com.android.delicieuxapp.model.Reqres
 import com.androidnetworking.AndroidNetworking
 import com.androidnetworking.error.ANError
 import com.androidnetworking.interfaces.ParsedRequestListener
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.detail_resto.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class MenuResto() : AppCompatActivity() {
-        //private var articles: MutableList<com.android.delicieuxapp.model.Restaurant> = mutableListOf()
-        //private lateinit var myAdapter: MyAdapter
-        //private lateinit var editsearch: EditText
+class MenuResto : AppCompatActivity() {
+    var b : Bundle? = null
+    //    private lateinit var linearLayoutManager: LinearLayoutManager
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        headerapicall()
+        setContentView(R.layout.menu_resto)
+//    linearLayoutManager = LinearLayoutManager(this)
+//    recyclerView.layoutManager = linearLayoutManager
+    }
+    fun headerapicall(){
+        b = intent.extras
+        var id = b?.getInt("id")
+        if (id != null) {
+            Api.service<RestaurantInfoService>()
+                    .getResInfo(id)
+                    .enqueue(object : Callback<RestoDetailResponse> {
+                        override fun onResponse(
+                                call: Call<RestoDetailResponse>,
+                                response: Response<RestoDetailResponse>
+                        ) {
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.menu_resto)
+                            tv_title_name.text = response.body()?.ResName
+                            var location= response.body()?.ResLocData?.ResLocname + " , " + response.body()?.ResLocData?.ResCity
+                            tv_title_loc.text= location
+                            var titleJenis = response.body()?.ResJenis?.get(0) + " - " + response.body()?.ResCuisines
+                            tv_title_type.text=titleJenis
 
-
-            //setup android networking
-            AndroidNetworking.initialize(this)
-
-            AndroidNetworking.get("https://developers.zomato.com/api/v2.1/search?apikey=0d9669f4a2ef9bab2589dda088256b93")
-                    .build()
-                    .getAsObject(Reqres::class.java, object: ParsedRequestListener<Reqres> {
-                        override fun onResponse(response: Reqres) {
+                            var rating = response.body()?.ResRating?.ResAngkaRating
+                            rb_title_star.rating= rating?.toFloat()!!
+                            if (rating=="0"){
+                                rating="No Rating"
+                            }
+                            tv_title_rating.text=rating
+                            if(response.body()?.ResPhotoUrl == ""){
+                                Picasso.get().load("https://krenova.bp3d.boyolali.go.id/images/no-image-available.jpg"
+                                ).into(iv_title)
+                            } else {
+                                Picasso.get().load(response.body()?.ResPhotoUrl).fit().centerCrop().into(iv_title)
+                            }
                         }
 
-                        override fun onError(anError: ANError?) {
+                        override fun onFailure(call: Call<RestoDetailResponse>, t: Throwable) {
                         }
-
                     })
-
         }
 
+    }
     }
