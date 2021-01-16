7971277561e61d1b38188029765a845a2bd979e1
Ammar3012 <50088752+Ammar3012@users.noreply.github.com>
2021-01-16 15:45:44 +0700

    bottombar jalan

---
+++ app/src/main/java/com/android/delicieuxapp/HomeFragment.kt
@@ -0,0 +1,100 @@
+package com.android.delicieuxapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.OrientationHelper
+import com.android.delicieuxapp.model.Reqres
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.ParsedRequestListener
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class HomeFragment : Fragment() {
+    lateinit var myAdapter: MyAdapter
+    val lm = LinearLayoutManager(activity)
+    var articles: MutableList<com.android.delicieuxapp.model.Restaurant> = mutableListOf()
+    lateinit var editTextSearch: EditText
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+        AndroidNetworking.initialize(activity)
+
+
+        AndroidNetworking.get(
+            "https://developers.zomato.com/api/v2.1/search?apikey=0d9669f4a2ef9bab2589dda088256b93")
+            .build()
+            .getAsObject(Reqres::class.java, object: ParsedRequestListener<Reqres> {
+                override fun onResponse(response: Reqres) {
+                    articles.addAll(response.restaurants)
+                    myAdapter.notifyDataSetChanged()
+                }
+
+                override fun onError(anError: ANError?) {
+                }
+
+            })
+
+        editTextSearch = searchEt
+        editTextSearch.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                filterList(s.toString());
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+
+
+
+        })
+
+        bt_sign.setOnClickListener {
+            val intent = Intent(activity, login::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+
+    fun initView(){
+        myAdapter = MyAdapter(articles)
+        recycler_view.layoutManager = lm
+        recycler_view.addItemDecoration(DividerItemDecoration(activity, OrientationHelper.VERTICAL))
+        recycler_view.adapter = myAdapter
+
+
+    }
+    private fun filterList(filterItem: String) {
+
+          var tempList: MutableList<com.android.delicieuxapp.model.Restaurant> = ArrayList()
+
+           for (d in articles){
+
+                if(filterItem in d.restaurant.name){
+                    tempList.add(d)
+
+                }
+           }
+              myAdapter.updateList(tempList)
+          }
+
+}
--- app/src/main/java/com/android/delicieuxapp/MainActivity.kt
+++ app/src/main/java/com/android/delicieuxapp/MainActivity.kt
@@ -10,6 +10,7 @@ import android.widget.EditText
 import android.widget.SearchView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.OrientationHelper
@@ -21,43 +22,51 @@ import com.android.delicieuxapp.model.Reqres
 import com.androidnetworking.AndroidNetworking
 import com.androidnetworking.error.ANError
 import com.androidnetworking.interfaces.ParsedRequestListener
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.detail_resto.*
+import kotlinx.android.synthetic.main.fragment_home.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 
 class MainActivity() : AppCompatActivity(){
-    private var articles: MutableList<com.android.delicieuxapp.model.Restaurant> = mutableListOf()
-    private lateinit var myAdapter: MyAdapter
-    private lateinit var editTextSearch: EditText
+  //  private var articles: MutableList<com.android.delicieuxapp.model.Restaurant> = mutableListOf()
+  //  private lateinit var myAdapter: MyAdapter
+  //  private lateinit var editTextSearch: EditText
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        bottomBar1.setOnNavigationItemSelectedListener(onBottomNavListener)
+
+        var fr = supportFragmentManager.beginTransaction()
+        fr.add(R.id.flFragment2, HomeFragment())
+        fr.commit()
+
         // setup adapter
-        myAdapter = MyAdapter(articles)
+       // myAdapter = MyAdapter(articles)
 
 
 
 
         // setup recycler_view
-        recycler_view.layoutManager = LinearLayoutManager(this)
-        recycler_view.addItemDecoration(DividerItemDecoration(this, OrientationHelper.VERTICAL))
-        recycler_view.adapter = myAdapter
+       // recycler_view.layoutManager = LinearLayoutManager(this)
+       // recycler_view.addItemDecoration(DividerItemDecoration(this, OrientationHelper.VERTICAL))
+       // recycler_view.adapter = myAdapter
 
-        editTextSearch = findViewById(R.id.searchEt)
+       // editTextSearch = findViewById(R.id.searchEt)
 
         // Search Button
-        editTextSearch.addTextChangedListener(object: TextWatcher{
+        /*editTextSearch.addTextChangedListener(object: TextWatcher{
             override fun afterTextChanged(s: Editable?) {
                 filterList(s.toString());
             }
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+           override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 
             }
 
@@ -68,30 +77,30 @@ class MainActivity() : AppCompatActivity(){
 
 
 
-        })
+        })*/
        // var id = searchEt.query.toString()
         //setup android networking
-        AndroidNetworking.initialize(this)
+      //  AndroidNetworking.initialize(this)
 
 
-        AndroidNetworking.get(
-            "https://developers.zomato.com/api/v2.1/search?apikey=0d9669f4a2ef9bab2589dda088256b93")
-            .build()
-            .getAsObject(Reqres::class.java, object: ParsedRequestListener<Reqres> {
-                override fun onResponse(response: Reqres) {
-                    articles.addAll(response.restaurants)
-                    myAdapter.notifyDataSetChanged()
-                }
+     //   AndroidNetworking.get(
+       //     "https://developers.zomato.com/api/v2.1/search?apikey=0d9669f4a2ef9bab2589dda088256b93")
+        //    .build()
+        //    .getAsObject(Reqres::class.java, object: ParsedRequestListener<Reqres> {
+         //       override fun onResponse(response: Reqres) {
+        //            articles.addAll(response.restaurants)
+         //           myAdapter.notifyDataSetChanged()
+         //       }
 
-                override fun onError(anError: ANError?) {
-                }
+         //       override fun onError(anError: ANError?) {
+        //        }
 
-            })
+        //    })
 
-        bt_sign.setOnClickListener {
-            val intent = Intent(this, login::class.java)
-            startActivity(intent)
-        }
+     //   bt_sign.setOnClickListener {
+     //       val intent = Intent(this, login::class.java)
+     //       startActivity(intent)
+     //   }
 
 
 
@@ -99,19 +108,37 @@ class MainActivity() : AppCompatActivity(){
 
     }
 
-    private fun filterList(filterItem: String) {
+    private val onBottomNavListener = BottomNavigationView.OnNavigationItemSelectedListener { i ->
+        var selectedFr: Fragment = HomeFragment()
 
-        var tempList: MutableList<com.android.delicieuxapp.model.Restaurant> = ArrayList()
+        when(i.itemId){
+            R.id.item_home -> {
+                selectedFr = HomeFragment()
+            }
 
-        for (d in articles){
-
-            if(filterItem in d.restaurant.name){
-                tempList.add(d)
-
+            R.id.item_profile -> {
+                selectedFr = AkunFragment()
             }
         }
-        myAdapter.updateList(tempList)
+        var fr = supportFragmentManager.beginTransaction()
+        fr.replace(R.id.flFragment2, selectedFr)
+        fr.commit()
+        true
     }
+
+   // private fun filterList(filterItem: String) {
+
+   //     var tempList: MutableList<com.android.delicieuxapp.model.Restaurant> = ArrayList()
+
+    //    for (d in articles){
+
+   //         if(filterItem in d.restaurant.name){
+   //             tempList.add(d)
+
+   //         }
+  //      }
+  //      myAdapter.updateList(tempList)
+  //  }
 
 
 }
