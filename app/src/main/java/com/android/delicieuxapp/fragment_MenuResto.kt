725c3381cdb6d41ec753764ada37bc3fc8951cec
daraldrlh <50088698+daraldrlh@users.noreply.github.com>
2021-01-19 23:56:19 +0700

    inflate

--- app/src/main/java/com/android/delicieuxapp/fragment_MenuResto.kt
+++ app/src/main/java/com/android/delicieuxapp/fragment_MenuResto.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.View.inflate
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.graphics.drawable.DrawableCompat.inflate
 import androidx.fragment.app.Fragment
 import com.android.delicieuxapp.API.*
 import com.squareup.picasso.Picasso
