2bd4f295fe665eeb746f693b21d5b80e8c31baaf
Ammar3012 <50088752+Ammar3012@users.noreply.github.com>
2021-01-19 16:54:11 +0700

    sign out done

--- app/src/main/java/com/android/delicieuxapp/AkunFragment.kt
+++ app/src/main/java/com/android/delicieuxapp/AkunFragment.kt
@@ -1,18 +1,55 @@
 package com.android.delicieuxapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
+import com.android.delicieuxapp.model.Reqres
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.ParsedRequestListener
+import com.google.firebase.auth.FirebaseAuth
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_akun.*
+import kotlinx.android.synthetic.main.fragment_home.*
 
-
+private lateinit var mAuth: FirebaseAuth
 class AkunFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_akun, container, false)
-    }
 
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+       mAuth = FirebaseAuth.getInstance()
+        val currentUser = mAuth.currentUser
+
+        if (currentUser != null) {
+            email.setText(currentUser.email)
+            name.setText(currentUser.displayName)
+        } else {
+            Toast.makeText(context, "Data Not Found", Toast.LENGTH_LONG).show()
+        }
+
+
+        Picasso.get().load(currentUser?.photoUrl).into(profile_image)
+
+        sign_out_btn.setOnClickListener {
+            mAuth.signOut()
+            val intent = Intent(activity, login::class.java)
+            startActivity(intent)
+            activity?.finish()
+        }
+
+    }
 
 }
