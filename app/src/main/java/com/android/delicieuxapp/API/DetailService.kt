ac007efc5981262f72d193c3e61d2caa82ccd5b0
Jessicawie <jwienadi@gmail.com>
2021-01-20 21:05:37 +0700

    .

--- app/src/main/java/com/android/delicieuxapp/API/DetailService.kt
+++ app/src/main/java/com/android/delicieuxapp/API/DetailService.kt
@@ -5,7 +5,7 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-//BELUM DIUBAH SAMSEK, BUAT GET DATA(valu yg di pass di url nanti)
+
 interface RestaurantInfoService {
     @GET("restaurant")
         fun getResInfo(
